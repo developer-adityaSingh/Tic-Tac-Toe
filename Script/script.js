166193d187efc553082e057aaeb175e81b2ad5f1
Aditya Singh <1211821022@ngpp.ac.in>
2024-08-02 21:47:41 +0530

    Changed Size of container for mobile screen

--- Script/script.js
+++ Script/script.js
@@ -1,23 +1,23 @@
 let check = 0
-let lastIdNode= box1
+let lastIdNode = box1
 function winnerCheck() {
     if (((document.getElementById("box1").innerHTML == "x") && (document.getElementById("box2").innerHTML == "x") && (document.getElementById("box3").innerHTML == "x")) || ((document.getElementById("box4").innerHTML == "x") && (document.getElementById("box5").innerHTML == "x") && (document.getElementById("box6").innerHTML == "x")) || ((document.getElementById("box7").innerHTML == "x") && (document.getElementById("box8").innerHTML == "x") && (document.getElementById("box9").innerHTML == "x")) || ((document.getElementById("box1").innerHTML == "x") && (document.getElementById("box5").innerHTML == "x") && (document.getElementById("box9").innerHTML == "x")) || ((document.getElementById("box3").innerHTML == "x") && (document.getElementById("box5").innerHTML == "x") && (document.getElementById("box7").innerHTML == "x")) || ((document.getElementById("box1").innerHTML == "x") && (document.getElementById("box4").innerHTML == "x") && (document.getElementById("box7").innerHTML == "x")) || ((document.getElementById("box2").innerHTML == "x") && (document.getElementById("box5").innerHTML == "x") && (document.getElementById("box8").innerHTML == "x")) || ((document.getElementById("box3").innerHTML == "x") && (document.getElementById("box6").innerHTML == "x") && (document.getElementById("box9").innerHTML == "x"))) {
-        let winnerId= document.getElementById(`${lastIdNode}`).innerHTML
-        let conf= confirm(`X is winner\nDo you want to continue`)
-        if(conf == true){
+        let winnerId = document.getElementById(`${lastIdNode}`).innerHTML
+        let conf = confirm(`X is winner\nDo you want to continue`)
+        if (conf == true) {
             window.location.reload()
         }
     }
     else if (((document.getElementById("box1").innerHTML == "o") && (document.getElementById("box2").innerHTML == "o") && (document.getElementById("box3").innerHTML == "o")) || ((document.getElementById("box4").innerHTML == "o") && (document.getElementById("box5").innerHTML == "o") && (document.getElementById("box6").innerHTML == "o")) || ((document.getElementById("box7").innerHTML == "o") && (document.getElementById("box8").innerHTML == "o") && (document.getElementById("box9").innerHTML == "o")) || ((document.getElementById("box1").innerHTML == "o") && (document.getElementById("box5").innerHTML == "o") && (document.getElementById("box9").innerHTML == "o")) || ((document.getElementById("box3").innerHTML == "o") && (document.getElementById("box5").innerHTML == "o") && (document.getElementById("box7").innerHTML == "o")) || ((document.getElementById("box1").innerHTML == "o") && (document.getElementById("box4").innerHTML == "o") && (document.getElementById("box7").innerHTML == "o")) || ((document.getElementById("box2").innerHTML == "o") && (document.getElementById("box5").innerHTML == "o") && (document.getElementById("box8").innerHTML == "o")) || ((document.getElementById("box3").innerHTML == "o") && (document.getElementById("box6").innerHTML == "o") && (document.getElementById("box9").innerHTML == "o"))) {
-        let winnerId= document.getElementById(`${lastIdNode}`).innerHTML
-        let conf= confirm(`O is winner\nDo you want to continue`)
-        if(conf == true){
+        let winnerId = document.getElementById(`${lastIdNode}`).innerHTML
+        let conf = confirm(`O is winner\nDo you want to continue`)
+        if (conf == true) {
             window.location.reload()
         }
     }
-    else if(((document.getElementById("box1").innerHTML == "o") || (document.getElementById("box1").innerHTML == "x")) && ((document.getElementById("box2").innerHTML == "o") || (document.getElementById("box2").innerHTML == "x")) && ((document.getElementById("box3").innerHTML == "o") || (document.getElementById("box4").innerHTML == "x")) && ((document.getElementById("box5").innerHTML == "o") || (document.getElementById("box5").innerHTML == "x")) && ((document.getElementById("box6").innerHTML == "o") || (document.getElementById("box6").innerHTML == "x")) && ((document.getElementById("box7").innerHTML == "o") || (document.getElementById("box7").innerHTML == "x")) && ((document.getElementById("box8").innerHTML == "o") || (document.getElementById("box8").innerHTML == "x")) && ((document.getElementById("box9").innerHTML == "o") || (document.getElementById("box9").innerHTML == "x"))){
-        let conf= confirm(`Match Draw`)
-        if(conf == true){
+    else if (((document.getElementById("box1").innerHTML == "o") || (document.getElementById("box1").innerHTML == "x")) && ((document.getElementById("box2").innerHTML == "o") || (document.getElementById("box2").innerHTML == "x")) && ((document.getElementById("box3").innerHTML == "o") || (document.getElementById("box4").innerHTML == "x")) && ((document.getElementById("box5").innerHTML == "o") || (document.getElementById("box5").innerHTML == "x")) && ((document.getElementById("box6").innerHTML == "o") || (document.getElementById("box6").innerHTML == "x")) && ((document.getElementById("box7").innerHTML == "o") || (document.getElementById("box7").innerHTML == "x")) && ((document.getElementById("box8").innerHTML == "o") || (document.getElementById("box8").innerHTML == "x")) && ((document.getElementById("box9").innerHTML == "o") || (document.getElementById("box9").innerHTML == "x"))) {
+        let conf = confirm(`Match Draw`)
+        if (conf == true) {
             window.location.reload()
         }
     }
@@ -39,22 +39,22 @@ document.querySelectorAll(".boxes").forEach((e) => {
         else {
             if (check == 0) {
                 event.target.innerHTML = "x"
-                document.querySelector(".msgBox").innerHTML= "<p>It's O Tern</p>"
+                document.querySelector(".msgBox").innerHTML = "<p>It's O Tern</p>"
                 check = 1
-                lastIdNode= event.target.id
-                setTimeout(()=>{
+                lastIdNode = event.target.id
+                setTimeout(() => {
                     winnerCheck()
-                },30)
+                }, 30)
             }
 
             else if (check == 1) {
                 event.target.innerHTML = "o"
-                document.querySelector(".msgBox").innerHTML= "<p>It's X Tern</p>"
+                document.querySelector(".msgBox").innerHTML = "<p>It's X Tern</p>"
                 check = 0
-                lastIdNode= event.target.id
-                setTimeout(()=>{
+                lastIdNode = event.target.id
+                setTimeout(() => {
                     winnerCheck()
-                },30)
+                }, 30)
             }
         }
     })
